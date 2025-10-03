546398f30aabe551ebc327a2d97f104e944b6122
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-10-03 08:57:14 +0330

    update

--- commandline/career/management/commands/rmCompany.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from career.models import Company
-
-
-class Command(BaseCommand):
-    pass
--- commandline/career/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.validators import RegexValidator
-
-
-PhoneValidator = RegexValidator(
-    regex=r'^(0|\+98|0098)9[0-9]{9}$',
-    message='Phone number format is not valid.',
-    code='invalid_phone_number'
-)
